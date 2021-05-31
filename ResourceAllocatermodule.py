96093bd73b6d7e95bb9bad8c6ab34dab2b29ec73
Harivyas S <81221672+HarivyasS@users.noreply.github.com>
2021-05-31 12:13:12 +0530

    Update ResourceAllocatermodule.py

--- ResourceAllocatermodule.py
+++ ResourceAllocatermodule.py
@@ -102,8 +102,23 @@ def get_costs(instances,hours,cpus,price):
 	print (allNodes)
 
 if __name__ == "__main__":
-	instances={'us-east': {'large': 0.12,'xlarge': 0.23,'2xlarge': 0.45,'4xlarge': 0.774,'8xlarge': 1.4,'10xlarge': 2.82},
-            'us-west': {'large': 0.14,'2xlarge': 0.413,'4xlarge': 0.89,'8xlarge': 1.3,'10xlarge': 2.97}}
+	instances={'us-east': 
+		   {'large': 0.12,
+		    'xlarge': 0.23,
+		    '2xlarge': 0.45,
+		    '4xlarge': 0.774,
+		    '8xlarge': 1.4,
+		    '10xlarge': 2.82
+		   },
+                    'us-west': 
+		   {
+		    'large': 0.14,
+		    '2xlarge': 0.413,
+		    '4xlarge': 0.89,
+	            '8xlarge': 1.3,
+		    '10xlarge': 2.97
+		   }
+		  }
 	hours = input("Hours:")
 	cpus = input("Cpus:")
 	price = float(input("Price:"))
