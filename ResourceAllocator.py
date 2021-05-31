72123c382984824252fe9d4d3a4a26315f393695
Harivyas S <81221672+HarivyasS@users.noreply.github.com>
2021-05-31 07:57:27 +0530

    Update ResourceAllocator.py

--- ResourceAllocator.py
+++ ResourceAllocator.py
@@ -7,13 +7,16 @@ Created on Sun May 30 07:32:27 2021
 
 server={"large":1,"xlarge":2,"2xlarge":4,"4xlarge":8,"8xlarge":16,"10xlarge":32}
 #print(server)
+instances={'us-east': {'large': 0.12,'xlarge': 0.23,'2xlarge': 0.45,'4xlarge': 0.774,'8xlarge': 1.4,'10xlarge': 2.82},
+            'us-west': {'large': 0.14,'2xlarge': 0.413,'4xlarge': 0.89,'8xlarge': 1.3,'10xlarge': 2.97}}
 
+#print(instances)
 output_list=[]
-def get_costs(hours, cpus, price):
+def get_costs(instances, hours, cpus, price):
     if(price == None):
-        get_price(hours,cpus)
+        get_price(instances,hours,cpus)
     if(cpus == None):
-        get_cpus(hours,price)
+        get_cpus(instances,hours,price)
 def print_output(region,cost,servers):
     output={}
     output["region"]=region
@@ -21,9 +24,7 @@ def print_output(region,cost,servers):
     output["servers"]=servers[::-1]
     output_list.append(output)
 #Alice would like to request for servers with minimum 135 CPUs for 24 hours.
-def get_price(hours,cpus):
-    instances={'us-east': {'large': 0.12,'xlarge': 0.23,'2xlarge': 0.45,'4xlarge': 0.774,'8xlarge': 1.4,'10xlarge': 2.82},
-            'us-west': {'large': 0.14,'2xlarge': 0.413,'4xlarge': 0.89,'8xlarge': 1.3,'10xlarge': 2.97}}
+def get_price(instances,hours,cpus):
     temp_cpu=cpus
     for region,servers in instances.items():
         cpus=temp_cpu
@@ -42,9 +43,7 @@ def get_price(hours,cpus):
         
         print_output(region,total*hours,server_list)
 #Bob would like to request as many possible servers for $38 for 10 hours.        
-def get_cpus(hours,price):
-    instances={'us-east': {'large': 0.12,'xlarge': 0.23,'2xlarge': 0.45,'4xlarge': 0.774,'8xlarge': 1.4,'10xlarge': 2.82},
-            'us-west': {'large': 0.14,'2xlarge': 0.413,'4xlarge': 0.89,'8xlarge': 1.3,'10xlarge': 2.97}}
+def get_cpus(instances,hours,price):
     for region,servers in instances.items():
         priceperhour=price/hours
         total=0
@@ -63,8 +62,8 @@ def get_cpus(hours,price):
         print_output(region,total*hours,server_list)        
 
 #Example1
-#get_costs(24,135,None)
+#get_costs(instances,24,135,None)
 #Example2
-#get_costs(10,None,38)
+#get_costs(instances,10,None,38)
 
 print(output_list)             
